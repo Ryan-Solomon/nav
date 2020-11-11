dc4878630ec401b6cdf050657981649274fe89f3
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-10 20:31:57 -0600

    init commit

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.styles.css';
+
+const NavBar = () => {
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>Products</li>
+        <li>Developers</li>
+        <li>Company</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
